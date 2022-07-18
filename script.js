b30f239c1e1124425d8dec9d67e9d231cf5888c9
Newton <altontonnalumasa@gmail.com>
2022-07-19 02:06:21 +0300

    use addeventlistener() method to sort javascript files in html

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 let checkbox = document.querySelector('.navigation__checkbox');
-let menu = document.querySelector('.menu');
-checkbox.addEventListener("click", function () {
+let menu = document.querySelector('.bar');
+menu.style.display==="block";
+checkbox.addEventListener("click", function() {
     if (menu.style.display === 'block') {
         menu.style.display = 'none';
     } else {
         menu.style.display = 'block';
     }
 });
+let closePort = document.querySelector(".closePort");
+closePort.addEventListener("click", close);
 
-function closeNav(){
-    document.getElementById("navi-toggle").checked = false;
-    let navOpen = document.querySelector('.menu');
-    if (navOpen.style.display === 'block') {
-        navOpen.style.display = 'none';
+let closeAbout = document.querySelector(".closeAbout");
+closeAbout.addEventListener("click", close);
+
+let closeContact = document.querySelector(".closeContact");
+closeContact.addEventListener("click", close);
+
+let closeEmail = document.querySelector(".closeEmail");
+closeEmail.addEventListener("click", close);
+
+let navToggle = document.getElementById("navi-toggle");
+
+function close(){
+    navToggle.checked = false;
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
     } else {
-        navOpen.style.display = 'block';
+        menu.style.display = 'flex';
     }
 }
