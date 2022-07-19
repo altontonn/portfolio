3de9217f5fd49f436f49b8ee13765562877d3b7f
Newton <altontonnalumasa@gmail.com>
2022-07-19 13:16:03 +0300

    fix js eslint errors

--- script.js
+++ script.js
@@ -2,32 +2,32 @@ const checkbox = document.querySelector('.navigation__checkbox');
 const menu = document.querySelector('.bar');
 
 checkbox.addEventListener('click', () => {
-  if (menu.style.display === 'block') {
-      menu.style.display = 'none';
-  } else {
-      menu.style.display = 'block';
+  if(menu.style.display === 'block') {
+    menu.style.display = 'none';
+  }else{
+    menu.style.display = 'block';
   }
 });
 
 const closePort = document.querySelector('closePort');
-closePort.addEventListener('click', close);
+closePort.addEventListener('click', closeNav);
 
 const closeAbout = document.querySelector('.closeAbout');
-closeAbout.addEventListener('click', close);
+closeAbout.addEventListener('click', closeNav);
 
 const closeContact = document.querySelector('.closeContact');
-closeContact.addEventListener('click', close);
+closeContact.addEventListener('click', closeNav);
 
 const closeEmail = document.querySelector('.closeEmail');
-closeEmail.addEventListener('click', close);
+closeEmail.addEventListener('click', closeNav);
 
 const navToggle = document.getElementById('navi-toggle');
 
-function close(){
+function closeNav() {
   navToggle.checked = false;
-  if (menu.style.display === 'flex') {
-      menu.style.display = 'none';
-  } else {
-      menu.style.display = 'flex';
+  if(menu.style.display === 'flex') {
+    menu.style.display = 'none';
+  }else{
+    menu.style.display = 'flex';
   }
 }
