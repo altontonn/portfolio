cb9d584a183ccaecbde867afe2e49b0bc0b3b692
Newton <altontonnalumasa@gmail.com>
2022-07-19 13:10:15 +0300

    fix js eslint errors

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-let checkbox = document.querySelector('.navigation__checkbox');
-let menu = document.querySelector('.bar');
-menu.style.display==="block";
-checkbox.addEventListener("click", function() {
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
+const checkbox = document.querySelector('.navigation__checkbox');
+const menu = document.querySelector('.bar');
+
+checkbox.addEventListener('click', () => {
+  if (menu.style.display === 'block') {
+      menu.style.display = 'none';
+  } else {
+      menu.style.display = 'block';
+  }
 });
-let closePort = document.querySelector(".closePort");
-closePort.addEventListener("click", close);
 
-let closeAbout = document.querySelector(".closeAbout");
-closeAbout.addEventListener("click", close);
+const closePort = document.querySelector('closePort');
+closePort.addEventListener('click', close);
 
-let closeContact = document.querySelector(".closeContact");
-closeContact.addEventListener("click", close);
+const closeAbout = document.querySelector('.closeAbout');
+closeAbout.addEventListener('click', close);
 
-let closeEmail = document.querySelector(".closeEmail");
-closeEmail.addEventListener("click", close);
+const closeContact = document.querySelector('.closeContact');
+closeContact.addEventListener('click', close);
 
-let navToggle = document.getElementById("navi-toggle");
+const closeEmail = document.querySelector('.closeEmail');
+closeEmail.addEventListener('click', close);
+
+const navToggle = document.getElementById('navi-toggle');
 
 function close(){
-    navToggle.checked = false;
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
+  navToggle.checked = false;
+  if (menu.style.display === 'flex') {
+      menu.style.display = 'none';
+  } else {
+      menu.style.display = 'flex';
+  }
 }
